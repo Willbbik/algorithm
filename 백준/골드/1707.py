d69d717545d6d93a7f4eebe1836d1c388e99da2b
minsoo <minsoo651@naver.com>
2025-01-22 23:40:31 +0900

    1707 푸는데, 로컬에서는 되는데 제출하면 시간초과남

--- 백준/골드/1707.py
+++ 백준/골드/1707.py
@@ -1,13 +1,8 @@
 from collections import deque
 
-with open("./data.txt", "r") as r:
-    data = r.read().strip().split("\n")
-    
-K = int(data[0])
-# K = int(input())
-result = []
-
-# 연결된 정점은 같은 색이 아니어야함
+# with open("./data.txt", "r") as file:
+#     def input():
+#         return file.readline().strip()
 
 def bfs(value):
     Q = deque()
@@ -17,44 +12,44 @@ def bfs(value):
     while Q:
         a = Q.popleft()
 
-        for i in maps[a]:
+        for z in maps[a]:
             
             # 첫 방문이라면 원본 노드의 반대 색상
-            if(colors[i] == -1):
-                Q.append(i)
-                colors[i] = 1- colors[a]
+            if(colors[z] == -1):
+                Q.append(z)
+                # 현재 노드가 1이면, 1-1 == 0
+                # 현재 노드가 0이면, 1-0 == 1
+                # 즉 현재노드와 다른 값을 가지게 할수있음
+                colors[z] = 1 - colors[a]
 
             # 방문을 했었는데 원본 노드와 같은 색이라면
-            elif(colors[i] != -1 and colors[i] == colors[a]):
+            # 인접한 노드가 같은 색이라면 이분그래프가 아님
+            elif(colors[z] == colors[a]):
                 return False
-            
-            # 방문을 했엇는데 원본 노드와 다른 색이라면
-            elif(colors[i] != -1 and colors[i] != colors[a]):
-                continue
     
     return True
 
+K = int(input())
 
-for k in range(K):
-    V, E = map(int, data[0+k].split(" "))
-    # V, E = map(int, input().split(" "))
+for _ in range(K):
+    V, E = map(int, input().split(" "))
 
-    maps = [[] for _ in range(V)]
-    colors = [-1] * V
+    maps = [[] for _ in range(V+1)]
+    colors = [-1] * (V+1)
     
-    for i in range(E):
-        a, b = map(int, data[0+k+(i+1)].split(" "))
-        # a, b = map(int, input().split(" "))
-        
-        maps[a].append(b)
-        maps[b].append(a)
+    # 인접리스트 구현
+    for _ in range(E):
+        u, v = map(int, input().split(" "))
+        maps[u].append(v)
+        maps[v].append(u)
     
-    for i in range(V):
+    # 전체 그래프 탐색
+    result = []
+    for i in range(1, V+1):
         if(colors[i] == -1):
-            bfs(i)
+            result.append(bfs(i))
     
-    if all(maps):
+    if all(result):
         print("YES")
     else:
         print("NO")
-
