4ef0153653ad975bdd5ea8adfc3b16369b4e9941
minsoo <minsoo651@naver.com>
2025-02-03 21:43:50 +0900

    백준 14501 python [퇴사]

--- 백준/실버/bruteforce/14501.py
+++ 백준/실버/bruteforce/14501.py
@@ -1,38 +1,37 @@
 import sys
 
-with open("./data.txt", "r") as file:
-    def input():
-        return file.readline().strip()
+# with open("./data.txt", "r") as file:
+#     def input():
+#         return file.readline().strip()
 
-# def input():
-#     return sys.stdin.readline()
+def input():
+    return sys.stdin.readline().strip()
 
-    N = int(input())
+N = int(input())
 
-    days = [0] * (N+1)
-    moneys = [0] * (N+1)
-    dp = [0] * (N+1)
+# N+1일째 되는날까지 금액 계산이 필요하기 때문에.
+dp = [0] * (N+1)
+days = [0] * (N)
+moneys = [0] * (N)
 
-    for i in range(1, N+1):
-        T, P = map(int, input().split())
-        days[i] = T
-        moneys[i] = P
+for i in range(0, N):
+    T, P = map(int, input().split())
+    days[i] = T
+    moneys[i] = P
 
+# N+1일째 되는날 완료되는 상담이 있는지 확인하기 위해서 N+1
+for i in range(N+1):
 
-    for i in range(1, N+1):
-
-        # 현재까지 할 수 있는 상담 개수 구하기.
-        arr = []
-        for j in range(1, N+1):
-            if(i == j + days[j] <= N):
-                arr.append(moneys[j])
-                
-        print(dp)
-        # 지금 2-7, 5-7에서 2-7을 선택해서 45가 아니라 50으로 나오는중
-        
-        if(arr):
-            dp[i] += dp[i-1] + max(arr)
-        else:
-            dp[i] = dp[i-1]
+    arr = []
+    for k in range(0, i):
+        if(k + days[k] <= i):
+            # 상담 완료 시점에
+            # 상담 시작일 금액 + 상담 시작일까지 모아뒀던 돈
+            arr.append(moneys[k] + dp[k])
     
-    print(dp)
+    if(arr):
+        dp[i] = max(arr)
+    else:
+        dp[i] = dp[i-1]
+
+print(max(dp))
