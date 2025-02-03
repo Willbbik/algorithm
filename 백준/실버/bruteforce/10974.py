ce6e2d79280f082251b0a9951843770354642276
minsoo <minsoo651@naver.com>
2025-02-03 22:52:34 +0900

    10974 수정

--- 백준/실버/bruteforce/10974.py
+++ 백준/실버/bruteforce/10974.py
@@ -1,24 +1,24 @@
 import sys
 
-with open("./data.txt", "r") as file:
-    def input():
-        return file.readline().strip()
+# with open("./data.txt", "r") as file:
+#     def input():
+#         return file.readline().strip()
 
-# def input():
-#     return sys.stdin.readline().strip()
+def input():
+    return sys.stdin.readline().strip()
 
-    N = int(input())
-    arr = []
+N = int(input())
+arr = []
+
+def backTracking():
+    if(len(arr) == N):
+        print(*arr)
+        return
     
-    def backTracking():
-        if(len(arr) == N):
-            print(arr)
-            return
-        
-        for a in range(1, N+1):
-            if a not in arr:
-                arr.append(a)
-                backTracking()
-                arr.pop()
+    for a in range(1, N+1):
+        if a not in arr:
+            arr.append(a)
+            backTracking()
+            arr.pop()
 
-    backTracking()
+backTracking()
