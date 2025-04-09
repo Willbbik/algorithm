8f13ed57d52afa11526562ac379c360fe710a1f4
minsoo <minsoo651@naver.com>
2025-04-09 23:23:39 +0900

    프로그래머스 카카오 17681 비밀지도

--- 프로그래머스/카카오/17681.py
+++ 프로그래머스/카카오/17681.py
@@ -1,14 +1,17 @@
 
 # 10진수 > 2진수 변환 (5자리 채워서)
-def to_binary_number(number):
-    binaryStr = str(bin(number))    
-    return binaryStr.replace("0b", "").zfill(5)
-
-
+def to_binary_number(number, n):    
+    return bin(number)[2:].zfill(n)
 
 def solution(n, arr1, arr2):
+    maps1 = [to_binary_number(num, n) for num in arr1]
+    maps2 = [to_binary_number(num, n) for num in arr2]
+    result = []
+
+    for row1, row2 in zip(maps1, maps2):
+        line = ''
+        for a, b in zip(row1, row2):
+            line += '#' if a == '1' or b == '1' else ' '
+        result.append(line)
     
-    maps = [[""] * n for __ in range(n)]
-    
-    for num in arr1:
-        to_binary_number(num)
+    return result
