9f5f8adfe63920a3b69563cd90f23de4a223d312
minsoo <minsoo651@naver.com>
2025-05-16 00:16:07 +0900

    python 1181

--- 백준/실버/sort/1181.py
+++ 백준/실버/sort/1181.py
@@ -1,20 +1,20 @@
 import sys
 
-with open("./data.txt", "r") as file:
-    def input():
-        return file.readline().strip()
+# with open("./data.txt", "r") as file:
+#     def input():
+#         return file.readline().strip()
     
-# def input():
-#     return sys.stdin.readline()
+def input():
+    return sys.stdin.readline().strip()
 
 
-    N = int(input())
-    arr = []
-    
-    for i in range(N):
-        arr.append(input())
-    
-    newArr = sorted(list(set(arr)), key=lambda x: (len(x), x))
-    
-    for word in newArr:
-        print(word)
+N = int(input())
+arr = []
+
+for i in range(N):
+    arr.append(input())
+
+newArr = sorted(list(set(arr)), key=lambda x: (len(x), x))
+
+for word in newArr:
+    print(word)
