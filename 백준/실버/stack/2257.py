988c18a324c195bf64ab7c9c0bfa953413b8032b
unknown <minsoo651@naver.com>
2026-01-23 11:04:04 +0900

    백준 2257 python stack

---
+++ 백준/실버/stack/2257.py
@@ -0,0 +1,33 @@
+import sys
+
+def input():
+    return sys.stdin.readline().strip()
+
+formula = list(input())
+stack = []
+
+for i in formula:
+    if i == '(':
+        stack.append(-1)
+    elif i == ')':
+        temp = 0
+
+        while stack:
+            a = stack.pop()
+            if a == -1:
+                break
+            else:
+                temp += a
+        stack.append(temp)
+        
+    elif i == 'H':
+        stack.append(1)
+    elif i == 'C':  
+        stack.append(12)
+    elif i == 'O':
+        stack.append(16)
+    else:
+        if stack:
+            stack.append(stack.pop() * int(i))
+
+print(sum(stack))
